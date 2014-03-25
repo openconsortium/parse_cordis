29cb3db08cc51de13b7f6a5b756dbfa05c71de6e
Peter Vanhee <peter.vanhee@gmail.com>
2014-03-25 12:42:47 +0100

    Add new script to process RCNs using API callbacks, utility script used for warming the API's cache

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ setup(
         "htmllaundry >= 2.0",
         "lxml >= 3.2.3",
     ],
-    scripts=['bin/parse_project', 'bin/parse_listing', 'bin/parse_listing_new'],
+    scripts=['bin/parse_project', 'bin/parse_listing', 'bin/parse_listing_new', 'bin/process_list_api'],
 )
